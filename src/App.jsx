874a46e182560e00454f948ab35612d6fab6ee1c
Tung <nguyenbuitung4@gmail.com>
2024-07-25 17:11:01 +0700

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Avatar, Button, Form, Input, List, Select } from "antd";
+import { Avatar, Button, Flex, Form, Input, List, message, Select } from "antd";
+import { ArrowUpOutlined } from "@ant-design/icons";
 import "./App.css";
 import * as signalR from "@microsoft/signalr";
 
@@ -45,6 +46,7 @@ function App() {
         console.log("Message received:", { user, message });
         setMessages((prevMessages) => [...prevMessages, { user, message }]);
       });
+      message.success("Đã vào phòng chat");
     }
   };
 
@@ -58,44 +60,84 @@ function App() {
   return (
     <div className="chatbox">
       <h1>Realtime Chat Nguyễn Bùi Tùng</h1>
+      <div style={{ overflow: "auto" }}>
+        
+
+        {messages.length > 0 && <div style={{display:'flex',justifyContent:"center"}}>
+          {/* <div className="menu">
+            <a href="#" className="back">
+              <i className="fa fa-angle-left" />{" "}
+              <img src="https://i.imgur.com/G4EjwqQ.jpg" draggable="false" />
+            </a>
+            <div className="name">Random chat</div>
+            <div className="members">
+              <b>You</b>, Marga, Charo &amp; Brotons
+            </div>
+          </div> */}
+          <ol className="chat">
+            {messages.map((item)=>(
+               <li className="other">
+               <div className="msg">
+                 <div className="user">
+                   {item.user}
+                 </div>
+                 <p>
+                  {item.message} 
+                 </p>
+                 <time>20:17</time>
+               </div>
+             </li>
+            ))}
+            
+            
+          </ol>
+          
+        </div>}
+      </div>
       <div className="formchat">
         {!chatRoom ? (
-          <Form
-            name="joinRoom"
-               layout="vertical"
-            style={{ width: 300 }}
-            initialValues={{ remember: true }}
-            onFinish={joinChatRoom}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Tên người dùng"
-              name="user"
-              rules={[
-                { required: true, message: "Vui lòng nhập tên của bạn!" },
-              ]}
+          <>
+            <Form
+              name="joinRoom"
+              layout="vertical"
+              style={{ width: 300 }}
+              initialValues={{ remember: true }}
+              onFinish={joinChatRoom}
+              autoComplete="off"
             >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Phòng"
-              name="chatRoom"
-              rules={[
-                { required: true, message: "Vui lòng chọn phòng!" },
-              ]}
-            >
-              <Select>
-                <Select.Option value="room1">Room 1</Select.Option>
-                <Select.Option value="room2">Room 2</Select.Option>
-                <Select.Option value="room3">Room 3</Select.Option>
-              </Select>
-            </Form.Item>
-            <Form.Item style={{textAlign:"center"}}>
-              <Button type="primary" htmlType="submit">
-                Tham gia
-              </Button>
-            </Form.Item>
-          </Form>
+              <div style={{ textAlign: "center" }}>
+                <img
+                  src="https://baoanjsc.com.vn/img/LOGO_BAOANJSC-Trang-KoSlogan.png"
+                  alt=""
+                />
+              </div>
+              <Form.Item
+                label="Tên người dùng"
+                name="user"
+                rules={[
+                  { required: true, message: "Vui lòng nhập tên của bạn!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Phòng"
+                name="chatRoom"
+                rules={[{ required: true, message: "Vui lòng chọn phòng!" }]}
+              >
+                <Select>
+                  <Select.Option value="room1">Room 1</Select.Option>
+                  <Select.Option value="room2">Room 2</Select.Option>
+                  <Select.Option value="room3">Room 3</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item style={{ textAlign: "center" }}>
+                <Button type="primary" htmlType="submit">
+                  Tham gia
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
         ) : (
           <>
             <Form
@@ -105,41 +147,31 @@ function App() {
               onFinish={onFinish}
               autoComplete="off"
             >
-              <Form.Item
-                label="Nội dung"
-                name="message"
-                rules={[
-                  { required: true, message: "Vui lòng nhập nội dung" },
-                ]}
-              >
-                <Input.TextArea />
-              </Form.Item>
-              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                  Send
-                </Button>
-              </Form.Item>
+              <Flex align="center" i>
+                <Form.Item
+                  style={{ width: 400 }}
+                  // label="Nội dung"
+                  name="message"
+                  rules={[
+                    { required: true, message: "Vui lòng nhập nội dung" },
+                  ]}
+                >
+                  <Input
+                    size="large"
+                    placeholder="large size"
+                    // suffix={<ArrowUpOutlined />}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button size="large" type="primary" htmlType="submit">
+                    Gửi <ArrowUpOutlined style={{ marginLeft: 8 }} />
+                  </Button>
+                </Form.Item>
+              </Flex>
             </Form>
           </>
         )}
       </div>
-      <List
-        itemLayout="horizontal"
-        dataSource={messages}
-        renderItem={(item, index) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={
-                <Avatar
-                  src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
-                />
-              }
-              title={item.user}
-              description={item.message}
-            />
-          </List.Item>
-        )}
-      />
     </div>
   );
 }
